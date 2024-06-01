27a0275e3c411550267e2adb5954196cc9a7e772
HelvinBasamaTondang86 <helpintondang@gmail.com>
2024-06-01 08:42:39 -0700

    crud_day14

---
+++ tugas_template/resources/views/cast/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('layout.master')
+
+
+@section('title')
+Halaman Edit Cast
+@endsection
+   
+@section('content')
+  
+<form action="{{ route('cast.update', $cast->id) }}" method="POST">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
+    @csrf
+    @method("put")
+    <div class="form-group">
+      <label >Nama Cast</label>
+      <input type="text" value="{{$cast->nama}}"class="form-control" name="nama">
+    </div>
+    <div class="form-group">
+      <label>Umur</label>
+      <input type="text"value="{{$cast->umur}}" class="form-control" name="umur">
+    </div>
+    <div class="form-group">
+        <label>Bio</label>
+        <textarea name="bio" class="form-control" cols="30" rows="10">{{$cast->bio}}"</textarea>
+      </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+@endsection
+ 
